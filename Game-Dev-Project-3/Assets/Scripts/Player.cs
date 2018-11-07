c2d84eca0ae5605b25623f81838beb6eb07ca978
Anthony <ieatbeens@gmail.com>
2018-11-07 16:07:50 -0500

    ¯\_(ツ)_/¯ It's making me add these so I can merge

---
+++ Game-Dev-Project-3/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void move(Vector3 move, bool jump)
+    {
+        body.AddForce(move);
+    }
+}
