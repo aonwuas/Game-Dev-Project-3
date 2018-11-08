5af7e7c3fa2eddc511314ed81feba5cc3e662ca6
Sophie Richards <richas3@rpi.edu>
2018-11-08 18:36:16 -0500

    Added basic health for the girl

---
+++ Assets/GirlHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlHealth : MonoBehaviour {
+	// put this on the girl's body, rn the health only changes when the cherry hits the body but not the hand
+	[SerializeField]
+	private int health=5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health==0){
+			Debug.Log("the girl is dead");
+			//change scene here(?)
+		}
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag=="Cherry"){
+			health-=1;
+		}
+	}
+}
