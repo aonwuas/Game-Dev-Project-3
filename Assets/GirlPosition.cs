c54ac612544c290409c00bd084f42cc9e693467c
Sophie Richards <richas3@rpi.edu>
2018-11-08 18:26:46 -0500

    Girl should now rotate around table instead of teleport

--- Assets/GirlPosition.cs
+++ Assets/GirlPosition.cs
@@ -7,54 +7,30 @@ public class GirlPosition : MonoBehaviour {
 	[SerializeField]
 	private GameObject player;
 	[SerializeField]
-	private Transform pos2;
+	private Transform table;
 	[SerializeField]
-	private Transform pos3;
-	[SerializeField]
-	private Transform pos4;
-	[SerializeField]
-	private Transform pos1;
-	private Vector3 position1;
-	private Vector3 position2;
-	private Vector3 position3;
-	private Vector3 position4;
+	private float distance=50;
+	private Vector3 direction;
 	private Vector3 playerPos;
-	private float[] magList;
+	private Vector3 center;
+	private Vector3 rot;
 
 	// Use this for initialization
 	void Start () {
-		magList=new float[4];
-		position1=transform.position;
-		position2=pos2.position;
-		position3=pos3.position;
-		position4=pos4.position;
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		center=table.transform.position;
 		playerPos=player.transform.position;
-		magList[0]=Vector3.Magnitude(playerPos-position1);
-		magList[1]=Vector3.Magnitude(playerPos-position2);
-		magList[2]=Vector3.Magnitude(playerPos-position3);
-		magList[3]=Vector3.Magnitude(playerPos-position4);
-		float min=Mathf.Min(magList);
-		if(min==magList[1]){
-			//the rotations will need to be adjusted once the model gets imported
-			transform.position=position2;
-			transform.rotation=Quaternion.Euler(0,90,0);
-		}
-		else if(min==magList[2]){
-			transform.rotation=Quaternion.Euler(0,0,0);
-			transform.position=position3;
-		}
-		else if(min==magList[3]){
-			transform.position=position4;
-			transform.rotation=Quaternion.Euler(0,90,0);
-		}
-		else{
-			transform.position=position1;
-			transform.rotation=Quaternion.Euler(0,0,0);
-		}
+		direction=playerPos-center;
+		direction.y=0;
+		direction=direction.normalized;
+		transform.position=direction*distance+center;
+		transform.LookAt(player.transform);
+		rot=transform.eulerAngles;
+		rot.x=0;
+		transform.eulerAngles=rot;
+		
 	}
 }
